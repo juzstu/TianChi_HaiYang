1efc90e9940dfb432f3abc0ddad588c487309d44
青禹小生 <laresh007@163.com>
2020-03-13 13:39:31 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:juzphy
+# datetime:2020/2/29 22:05 下午
+
+
+class Config:
+    train_dir = '/tcdata/hy_round2_train_20200225'
+    test_dir = '/tcdata/hy_round2_testA_20200225'
+    save_path = 'result.csv'
+    prob_qyxs = 'qyxs_prob.csv'
+    use_prob = False
+
+
+config = Config()
