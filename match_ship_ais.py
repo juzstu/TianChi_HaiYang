e51d545ee971030cf89a138a57f4a7a4c7d189b2
青禹小生 <laresh007@163.com>
2020-03-15 17:38:24 +0800

    Update match_ship_ais.py

--- match_ship_ais.py
+++ match_ship_ais.py
@@ -6,56 +6,91 @@ from utils import geohash_encode
 import os
 import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
-def match_ship_in_ais(train_file, ais_file, use_lat_lon=False):
+def match_ship_in_ais(train_file, ais_file, threshold, cnt_limit=50):
     paths = os.listdir(ais_file)
     match_df = pd.DataFrame()
 
-    # 复赛训练集合并文件
+    # 复赛训练集
     train = pd.read_csv(train_file)
     train.columns = ['ID', 'lat', 'lon', 'speed', 'direction', 'time', 'type']
+    train.sort_values(['ID', 'time'], inplace=True)
+    train['lat'] = train['lat'].round(2)
+    train['lon'] = train['lon'].round(2)
     print('origin train records num:', train.shape[0])
-    train['time'] = train['time'].apply(lambda x: x[:7])
+    train['time'] = train['time'].apply(lambda x:x[:4])
     train = train.drop_duplicates(subset=['ID', 'time', 'lat', 'lon'])
-
     print('after drop duplicates, train records num:', train.shape[0])
-    if not use_lat_lon:
-        train['geohash7'] = train.apply(lambda x: geohash_encode(x['lat'], x['lon'], 7), axis=1)
-    else:
-        train['lat'] = train['lat'].round(3)
-        train['lon'] = train['lon'].round(3)
+
     max_lat, min_lat, max_lon, min_lon = train['lat'].max(), train['lat'].min(), train['lon'].max(), train['lon'].min()
     print('*'*100)
+    train_agg = train.groupby('ID')['lat', 'lon'].agg(list).reset_index()
+    train_cnt = train.groupby(['ID'])['time'].agg({'cnt': 'count'}).reset_index()
+    train_cnt = train_cnt[train_cnt['cnt'] > cnt_limit]
+    train_agg = train_agg[train_agg['ID'].isin(train_cnt['ID'].unique())]
+    train_time = train.groupby('ID')['time', 'type'].agg('min').reset_index()
+    train_agg = train_agg.merge(train_time, on='ID', how='left')
+    train_agg['lon_lat'] = train_agg.apply(lambda x: {'{}_{}'.format(z[0], z[1]) for z in zip(x['lat'], x['lon'])}, axis=1)
+    del train_agg['lat'], train_agg['lon']
     for t in tqdm(range(len(paths))):
         p = paths[t]
         print(f'start match train ship with ais: {p}')
         ais = pd.read_csv(f'{ais_file}/{p}')
         ais.columns = ['ais_speed', 'ais_direction', 'ais_id', 'time', 'ais_lon', 'ais_lat']
+        ais.sort_values(['ais_id', 'time'], inplace=True)
         print('origin ais records num:', ais.shape[0])
-        ais['time'] = ais['time'].apply(lambda x: x[:7])
+        ais['time'] = ais['time'].apply(lambda x:x[:4])
+        ais['ais_lat'] = ais['ais_lat'].round(2)
+        ais['ais_lon'] = ais['ais_lon'].round(2)
         ais = ais.drop_duplicates(subset=['ais_id', 'time', 'ais_lat', 'ais_lon'])
         print('after drop duplicates, ais records num:', ais.shape[0])
-        ais = ais[(ais['ais_lat']>min_lat) & (ais['ais_lat']<max_lat) & (ais['ais_lon']>min_lon) & (ais['ais_lon']<max_lon)]
+        ais = ais[(ais['ais_lat'] > min_lat) & (ais['ais_lat'] < max_lat) & (ais['ais_lon'] > min_lon) & (ais['ais_lon'] < max_lon)]
         print('remove beyond threshold lat and lon, ais records num:', ais.shape[0])
-        if not use_lat_lon:
-            ais['geohash7'] = ais.apply(lambda x: geohash_encode(x['ais_lat'], x['ais_lon'], 7), axis=1)
-            merge_df = train.merge(ais, how='inner', on=['time', 'geohash7'])
-        else:
-            ais['ais_lat'] = ais['ais_lat'].round(3)
-            ais['ais_lon'] = ais['ais_lon'].round(3)
-            merge_df = train.merge(ais, how='inner', left_on=['time', 'lat', 'lon'], right_on=['time', 'ais_lat', 'ais_lon'])
-        merge_df['cnt'] = merge_df.groupby('ID')['ais_id'].transform('nunique')
-        merge_df = merge_df[merge_df['cnt'] == 1]
-        del merge_df['cnt']
-        print('now match records num:', merge_df['ID'].nunique())
-        match_df = match_df.append(merge_df)
-        print('total match records num:', match_df['ID'].nunique())
-        print('#'*100)
+        ais_cnt = ais.groupby(['ais_id'])['time'].agg({'cnt': 'count'}).reset_index()
+        ais_cnt = ais_cnt[ais_cnt['cnt'] > cnt_limit]
+        ais_time = ais.groupby('ais_id')['time'].agg('min').reset_index()
+        ais_agg = ais.groupby('ais_id')['ais_lat', 'ais_lon'].agg(list).reset_index()
+        ais_agg = ais_agg[ais_agg['ais_id'].isin(ais_cnt['ais_id'].unique())]
+        ais_agg = ais_agg.merge(ais_time, on='ais_id', how='left')
+        ais_agg['ais_lon_lat'] = ais_agg.apply(lambda x: {'{}_{}'.format(z[0], z[1]) for z in zip(x['ais_lat'], x['ais_lon'])}, axis=1)
+        del ais_agg['ais_lat'], ais_agg['ais_lon']
+        all_df = train_agg.merge(ais_agg, on='time', how='inner')
+        if all_df.shape[0] == 0:
+            continue
+        all_df['share_size'] = all_df.apply(lambda x: len(x['lon_lat'] & x['ais_lon_lat']), axis=1)
+        all_df['ratio_x'] = all_df.apply(lambda x: x['share_size']/len(x['ais_lon_lat']), axis=1)
+        all_df['ratio_y'] = all_df.apply(lambda x: x['share_size']/len(x['lon_lat']), axis=1)
+        all_df = all_df[(all_df['ratio_x'] >= threshold) & (all_df['ratio_y'] >= threshold)]
+        all_df['ais_file_name'] = p.split('.')[0]
+        match_df = match_df.append(all_df[['ID', 'type', 'ais_id', 'share_size', 'ratio_x', 'ratio_y', 'ais_file_name']])
+        print('now match:', match_df.shape[0])
+        print('*'*100)
     return match_df
+
+
+# 可视化匹配成功的数据，再做筛选
+def compare_plot(ids, ais_file):
+    map_net = {'刺网': 'cw', '围网': 'ww', '拖网': 'tw'}
+    fig = plt.figure(figsize=(8, 4))
+    plt.subplot(2, 2, 1)
+    train = pd.read_csv('train_round2.csv')
+    left = train[train['渔船ID'] == ids[0]]
+    left_type = left['type'].map(map_net).min()
+    plt.scatter(left['lat'], left['lon'], color='blue')
+    ais = pd.read_csv(f'{ais_file}/{ids[2]}.csv')
+    ais = ais[ais['ais_id'] == ids[1]]
+    plt.subplot(2, 2, 2)
+    plt.scatter(ais['lat'], ais['lon'], color='red')
+    fig.suptitle(f'match ID:{ids[0]}, type: {left_type}, ais_id:{ids[1]}, ais_file:{ids[2]}')
+    plt.show()
 
 
 if __name__ == "__main__":
     save_path = 'match.csv'
-    match_result = match_ship_in_ais('train_round2.csv', 'round2_ais_20200310')
+    ais_path = 'round2_ais_20200310'
+    match_result = match_ship_in_ais('train_round2.csv', ais_path, 0.5, cnt_limit=50)
     match_result.to_csv(save_path, index=False)
+    for m in match_result[['ID', 'ais_id', 'ais_file_name']].values:
+        compare_plot(m, ais_path)
